24fa6d16bfdc9b43cbd500710444f02338d54a99
BrianSammit <briansammit.cruz@gmail.com>
2021-09-02 22:11:32 -0500

    Fix error in the funtionality

--- javascript/index.js
+++ javascript/index.js
@@ -2,13 +2,13 @@ const burBtn = document.getElementById('burBtn');
 const closeBtn = document.getElementById('closeBtn');
 const mobileNav = document.getElementById('mobileNav');
 
-burBtn.addEventListener('click', () => {
+burBtn.addEventListener('click', function () {
   burBtn.classList.add('display-none');
   closeBtn.classList.remove('display-none');
   mobileNav.classList.remove('display-none');
 });
 
-closeBtn.addEventListener('click', () => {
+closeBtn.addEventListener('click', function () {
   burBtn.classList.remove('display-none');
   closeBtn.classList.add('display-none');
   mobileNav.classList.add('display-none');
